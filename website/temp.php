13dc67071af2a7887e9d80eafe07697cdcdf900d
SkY <51914435+SkYiMlTo@users.noreply.github.com>
2022-11-03 17:01:00 +0100

    Update temp.php

--- website/temp.php
+++ website/temp.php
@@ -6,6 +6,6 @@
     <meta http-equiv="refresh" content="0; URL=https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
 </head>
 <body>
-<p>This page has been moved. If you are not redirected within 3 seconds, click <a href="https://www.github.com/">here</a> to go to the HubSpot homepage.</p>
+
 </body>
 </html>
