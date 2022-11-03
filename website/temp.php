c62ab2d8ddc728fc1f5de75e5926070c39e17d38
Hugo Bourreau <hugo_bourreau@connect-tech.sncf>
2022-11-03 16:53:57 +0100

    rick ?

--- website/temp.php
+++ website/temp.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
     <title>Old Page</title>
     <meta charset="UTF-8" />
-    <meta http-equiv="refresh" content="3; URL=https://www.github.com/" />
+    <meta http-equiv="refresh" content="0; URL=https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
 </head>
 <body>
 <p>This page has been moved. If you are not redirected within 3 seconds, click <a href="https://www.github.com/">here</a> to go to the HubSpot homepage.</p>
