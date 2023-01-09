9be2687b60e43fcec7bc88c6f8d0ef9ebaca4c93
Hugo Bourreau <hugo_bourreau@connect-tech.sncf>
2023-01-09 15:00:10 +0100

    well..

--- website/index.php
+++ website/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <!-- CSS only -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <!--<meta http-equiv="refresh" content="0; URL=https://www.youtube.com/watch?v=dQw4w9WgXcQ" /> -->
+    <meta http-equiv="refresh" content="0; URL=https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
 </head>
 
 <body>
