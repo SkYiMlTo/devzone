95e54c35c709518de1009f79917a8d89be4560fb
Hugo <gogo.bourreau@hotmail.fr>
2023-02-09 16:58:46 +0100

    demo

--- website/index.php
+++ website/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <!-- CSS only -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <meta http-equiv="refresh" content="0; URL=https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
+<!--    <meta http-equiv="refresh" content="0; URL=https://www.youtube.com/watch?v=dQw4w9WgXcQ" />-->
 </head>
 
 <body>
@@ -17,6 +17,7 @@ include "./utilities/navbar-index.php"
     <h3>L'objectif de cette page</h3>
     <p style="margin-bottom: 2%;">Our cool objbectives here.</p>
     <h3 class="mt-3">Autre section si besoin</h3>
+    <p>Salut bg</p>
 </div>
 </body>
 
